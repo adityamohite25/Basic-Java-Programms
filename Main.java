50b9d92516b88e4f8e45a79ebf5ae2ebf8b2dd33
Aditya Mohite <mohiteaditya2003@gmail.com>
2023-02-16 18:42:40 +0530

    Sum of two numbers in Java

---
+++ Main.java
@@ -0,0 +1,31 @@
+// import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        //System.out.print("Enter your roll  number ");
+        // Scanner input = new Scanner(System.in);
+        // int a=input.nextInt();
+        // System.out.println("Your Roll Number is "+a);
+        // String name = input.next(); //next() only prints the first word of the
+        // //entire string 
+        // System.out.println(name);
+        // String namee=input.nextLine();
+        // System.out.println(namee);
+        // float marks=input.nextFloat();
+        // System.out.println("Marks is "+marks);
+        // int count = 0;
+        // while(count<11){
+        //     System.out.println(count*2);
+        //     count++;
+        // }
+        //    for(int i=0; i<21; i++){
+        //     System.out.println("18"+"X"+i+"="+i*18);
+        //    }
+            int i = 0;
+            while(i<11){
+                System.out.println("2"+"x"+i+"="+i*2);
+                i++;
+            }
+    }
+}
