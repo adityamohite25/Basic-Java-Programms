7f421900d0a0efd827be6f5ad0dd67cca13233ca
Aditya Mohite <mohiteaditya2003@gmail.com>
2023-02-18 12:11:05 +0530

    Finding out whether the alphabet is uppercase or lowercase by taking user input

---
+++ UpperLowercase.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class UpperLowercase{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    System.out.print("Enter a alphabet ");
+    char ch = in.next().trim().charAt(0);
+    if(ch>='a'&& ch<='z'){
+        System.out.println("Lowercase");
+    }
+    else{
+        System.out.println("Uppercase");
+    }
+
+    }
+
+}
