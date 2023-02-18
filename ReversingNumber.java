71c805fd4c71564411f8a6e8cdd66b6b71d04b2c
Aditya Mohite <mohiteaditya2003@gmail.com>
2023-02-18 11:59:55 +0530

    Reversing a number by taking user input

---
+++ ReversingNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ReversingNumber{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number ");
+        int num = in.nextInt();
+        int ans = 0;
+        while(num>0){
+            int rem = num%10;
+            num = num/10;
+            ans = ans*10+rem;
+        }
+
+        System.out.println(ans);
+
+    }
+
+}
